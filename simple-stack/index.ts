3c452a069c6c8cc06d0ab2177d35e59fd7180057
MitchellGerdisch <mitch@pulumi.com>
2022-11-02 15:20:25 -0500

    initial commit

---
+++ simple-stack/index.ts
@@ -0,0 +1,10 @@
+import * as pulumi from "@pulumi/pulumi";
+
+import * as random from "@pulumi/random";
+
+const config = new pulumi.Config();
+const petLength = config.getNumber("petLength") ?? 2;
+
+const randomPet = new random.RandomPet("random-pet", {
+  length: petLength
+})
