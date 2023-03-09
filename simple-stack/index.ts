249cf4565238be5cccf5380c6266f6a3fe60183a
Mitch Gerdisch <mitch@pulumi.com>
2023-03-09 14:13:31 -0600

    Add name separator

--- simple-stack/index.ts
+++ simple-stack/index.ts
@@ -4,10 +4,12 @@ import * as config from "./config";
 
 const nameLength = config.petLength
 const namePrefix = config.petNamePrefix
+const nameSeparator = config.petNameSeparator
 
 const randomPet = new random.RandomPet("random-pet", {
   length: nameLength,
-  prefix: namePrefix
+  prefix: namePrefix,
+  separator: nameSeparator
 });
 
 export const petName = randomPet.id;
