8d9a4d9ceac6880f2ce5aaa54e09893e915ecdca
Piers Karsenbarg <piers@pulumi.com>
2023-11-14 09:47:44 +0000

    updated again

--- aws-serverless/app/index.js
+++ aws-serverless/app/index.js
@@ -2,6 +2,6 @@ exports.handler =  async function(event, context) {
     console.log("EVENT: \n" + JSON.stringify(event, null, 2))
     return {
         statusCode: 200,
-        body: "Hello, World from Pulumi!"
+        body: "Hello, World from Pulumi and Piers!"
     };
   }
