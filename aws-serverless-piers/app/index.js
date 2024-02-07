6a085d16dec207fa0ea1c718b5eb59a8166b6f8a
Piers Karsenbarg <piers@pulumi.com>
2024-02-07 12:05:20 +0000

    Updated status code to 201

--- aws-serverless-piers/app/index.js
+++ aws-serverless-piers/app/index.js
@@ -1,7 +1,7 @@
 exports.handler =  async function(event, context) {
     console.log("EVENT: \n" + JSON.stringify(event, null, 2))
     return {
-        statusCode: 404,
+        statusCode: 201,
         body: "Hello, World from Pulumi!"
     };
   }
