878e11abfe616ed62539bf509bcdcedf775ec483
Piers Karsenbarg <piers@pulumi.com>
2024-08-01 11:54:14 +0100

    updating to trigger deployments

--- agent-k8s-test/index.ts
+++ agent-k8s-test/index.ts
@@ -1,8 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as random from "@pulumi/random";
 
-const pet = new random.RandomPet("pet", {
-    length: 2
-});
+const pet = new random.RandomPet("pet");
 
 export const petName = pet.id;
