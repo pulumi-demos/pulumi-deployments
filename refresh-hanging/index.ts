35f90ae0d42da2310f9883b22cd44950ee0f5a02
Piers Karsenbarg <piers@pulumi.com>
2025-05-02 12:29:39 +0100

    Added logs for PULUMI_RUN_PROGRAM value

--- refresh-hanging/index.ts
+++ refresh-hanging/index.ts
@@ -1,10 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as random from "@pulumi/random";
 
-for (let i: number = 0; i<=9; i++) {
+for (let i: number = 0; i<=10; i++) {
     const pw = new random.RandomPassword(`password-${i}`, {
         length: 21
     })
 }
 
 console.log("running program")
+console.log(`PULUMI_RUN_PROGRAM: ${process.env.PULUMI_RUN_PROGRAM}`)
