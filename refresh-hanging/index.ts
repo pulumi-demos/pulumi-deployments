4c1ca4ad3cdcdfc008a452dbedcea65ad5df553d
Piers Karsenbarg <piers@pulumi.com>
2025-05-02 11:35:27 +0100

    Setting number of resources to 7

--- refresh-hanging/index.ts
+++ refresh-hanging/index.ts
@@ -1,7 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as random from "@pulumi/random";
 
-for (let i: number = 0; i<=5; i++) {
+for (let i: number = 0; i<=7; i++) {
     const pw = new random.RandomPassword(`password-${i}`, {
         length: 21
     })
