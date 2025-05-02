fa7fb7ca9f702e9437e5aa994fdd7637bb2432d0
Piers Karsenbarg <piers@pulumi.com>
2025-05-02 11:31:27 +0100

    Decreased number of resources to 5

--- refresh-hanging/index.ts
+++ refresh-hanging/index.ts
@@ -1,11 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as random from "@pulumi/random";
 
-for (let i: number = 0; i<=10; i++) {
-    const pw = new random.RandomPassword(`-${i}`, {
+for (let i: number = 0; i<=5; i++) {
+    const pw = new random.RandomPassword(`password-${i}`, {
         length: 21
     })
 }
-
 
 console.log("running program")
